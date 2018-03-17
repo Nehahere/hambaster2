55e53cf1c60e7b9a1776c9705db1fc8e69f9f0ce
neha <yolomojo1900@gmail.com>
2018-03-17 22:42:38 +0530

    video no-18 and 19 Made an interactive app by adding event handling to button.. on click of button text changes.

---
+++ app/src/main/java/com/neha/hambaster/SmartButton.java
@@ -0,0 +1,33 @@
+package com.neha.hambaster;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class SmartButton extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_smart_button);
+
+
+        Button nehasbutton = (Button)findViewById(R.id.nehasbutton);
+
+        nehasbutton.setOnClickListener(
+                new Button.OnClickListener(){
+
+                    public  void onClick(View v){
+                        TextView nehastext =(TextView)findViewById(R.id.nehastext);
+                        nehastext.setText("Good Job Hoss");
+                    }
+                }
+        );
+    }
+}
+

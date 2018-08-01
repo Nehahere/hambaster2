392a96e9ce9f7448cc8c92aa8673f8477c98f85d
neha <yolomojo1900@gmail.com>
2018-08-01 12:39:28 +0530

    Video No:-39 and 40 Built an app using threads. Added a button and a text field and on click of button text changes after 10 seconds.

--- app/src/main/java/com/neha/hambaster/Lauren.java
+++ app/src/main/java/com/neha/hambaster/Lauren.java
@@ -5,8 +5,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.os.Handler;
+import android.os.Message;
+import android.widget.Button;
 
 public class Lauren extends AppCompatActivity {
+
+    Handler handle= new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            TextView nehasText=(TextView)findViewById(R.id.nehasText);
+            nehasText.setText("Nice Job Hoss");
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,15 +27,26 @@ public class Lauren extends AppCompatActivity {
     }
 
     public void clickNehasButton(View view){
-        long futureTime=System.currentTimeMillis()+10000;
-        while(System.currentTimeMillis() < futureTime){
-            synchronized (this){
-                try{
-                    wait(futureTime- System.currentTimeMillis());
-                }catch (Exception e){}
+
+        Runnable r=new Runnable() {
+            @Override
+            public void run() {
+                long futureTime=System.currentTimeMillis()+100000;
+                while(System.currentTimeMillis() < futureTime){
+                    synchronized (this){
+                        try{
+                            wait(futureTime-System.currentTimeMillis());
+                        }catch (Exception e){}
+                    }
+                }
             }
-        }
-        TextView nehasText=(TextView)findViewById(R.id.nehasText);
-        nehasText.setText("Good Job Hoss");
+        };
+
+        Thread thread=new Thread(r);
+        thread.start();
+
+        TextView nehasText = (TextView) findViewById(R.id.nehasText);
+        nehasText.setText("Nice Job Hoss!");
+
     }
 }

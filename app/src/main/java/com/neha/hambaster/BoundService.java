52c57b1a25217e3d07f7e168608cf6973331af58
neha <yolomojo1900@gmail.com>
2018-08-07 08:25:53 +0530

    Video No :- 44 Continued the last app n on click of button it shows current time

--- app/src/main/java/com/neha/hambaster/BoundService.java
+++ app/src/main/java/com/neha/hambaster/BoundService.java
@@ -1,6 +1,7 @@
 package com.neha.hambaster;
 
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -16,13 +17,34 @@ import com.neha.hambaster.MyService1.MyLocalBinder;
 public class BoundService extends AppCompatActivity {
 
     MyService1 nehaservice;
-    boolean iBound=false;
+    boolean isBound=false;
+
+    public void showTime(View view){
+        String currentTime=nehaservice.getCurrentTime();
+        TextView text=(TextView) findViewById(R.id.text);
+        text.setText(currentTime);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bound_service);
-
+        Intent i=new Intent(this,MyService1.class);
+        bindService(i,nehasConnection,Context.BIND_AUTO_CREATE);
     }
+
+    private ServiceConnection nehasConnection=new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder service) {
+            MyLocalBinder Binder=(MyLocalBinder)service;
+            nehaservice=Binder.getService();
+            isBound=true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            isBound=false;
+        }
+    };
 
 }
